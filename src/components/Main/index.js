ec120e0c62c6efcac775e3e13a47fe7d0deb5b7d
mahume <michael.a.hume@gmail.com>
2019-05-05 10:35:36 -0600

    Images center, grid set up still in progress

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -5,17 +5,19 @@ import './style.css';
 function Main({ images, handler }) {
   return (
     <main>
-      {images.map(image => (
-          <AvengersCard 
-            className="character" 
-            key={image.id} 
-            id={image.id}
-            name={image.name}
-            image={image.image} 
-            handler={handler}
-          ></AvengersCard>
-        )
-      )}
+      <div id="image-wrapper">
+        {images.map(image => (
+            <AvengersCard 
+              className="character" 
+              key={image.id} 
+              id={image.id}
+              name={image.name}
+              image={image.image} 
+              handler={handler}
+            ></AvengersCard>
+          )
+        )}
+      </div>
     </main>
   );
 }
